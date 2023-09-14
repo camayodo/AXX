0f8a68abb62fa3b50941010838c866bf818fd2a3
camayodo <144868305+camayodo@users.noreply.github.com>
2023-09-14 21:11:56 +0800

    Add files via upload
    assignment

---
+++ cal-of-1000.r
@@ -0,0 +1,5 @@
+n <- 1000
+
+sum_result <- n * (n + 1) / 2
+
+c("The sum of the first 100 positive integers is:", total)
---
+++ calculation-of-100.r
@@ -0,0 +1,6 @@
+n <- 100
+
+total <- n * (n + 1) / 2
+
+c("The sum of the first 100 positive integers is:", total)
+
